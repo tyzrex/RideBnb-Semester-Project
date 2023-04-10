46e794f5242f28844eed52cec9b0e28614121d3e
Sulav Baral <sulavbaral58@gmail.com>
2023-04-11 00:00:50 +0545

    correction

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import { notFound, errorHandler } from './src/middleware/authMiddleware.js';
 import AuthRoute from './src/routes/auth.js';
 
 const app = express();
@@ -15,10 +14,6 @@ app.use(express.json());
 
 app.use('/auth', AuthRoute)
 
-
-//Erorr handler middleware
-app.use(notFound);
-app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Rental app listening at http://localhost:${port}`);
