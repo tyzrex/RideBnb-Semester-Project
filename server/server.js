e57f45248630f84beef8b4f1134df4e475c58b9a
Sulav Baral <sulavbaral58@gmail.com>
2023-05-13 18:25:33 +0545

    new

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const port = 5000;
 app.use(
   cors({
     // origin: ["http://localhost:5173"],
-    origin: "https://ride-bnb.vercel.app",
+    origin: "https://ridebnb-frontend.onrender.com/",
     credentials: true,
   })
 );
