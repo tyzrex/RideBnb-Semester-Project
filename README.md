04d38b05698db802ef311f03cda320f2e499b14f
Sulav Baral <sulavbaral58@gmail.com>
2023-06-07 09:51:14 +0545

    bruh

--- README.md
+++
@@ -1,1 +0,0 @@
-# RideBnb-Semester-Project
--- src/routes/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { uploadImage } from "../controllers/cloudinary.js";
-
-//upload the vehicle image in the post to the server
-router.route("/uploadImage").post(uploadImage);
-
-export default router;
